7f306b1d62ba940422ec8f59370f03ee87ae7568
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2017-07-04 15:38:29 -0500

    Add endpoint to run ls.

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,26 @@ use iron::prelude::Response;
 use log::LogLevel;
 use logger::Logger;
 use router::Router;
+use std::process::Command;
 
-fn handler(_: &mut Request) -> IronResult<Response> {
+fn index_handler(_: &mut Request) -> IronResult<Response> {
   Ok(Response::with((iron::status::Ok, "Hello world!")))
+}
+
+fn ls_handler(_: &mut Request) -> IronResult<Response> {
+  let output = Command::new("ls")
+                        .arg("-l")
+                        .output()
+                        .expect("failed to execute process");
+  return Ok(Response::with((iron::status::Ok, String::from_utf8_lossy(&output.stdout).into_owned())));
 }
 
 fn main() {
   simple_logger::init_with_level(LogLevel::Info).unwrap();
 
   let mut router = Router::new();
-  router.get("/", handler, "index");
+  router.get("/", index_handler, "index");
+  router.get("/ls", ls_handler, "ls");
 
   let mut chain = Chain::new(router);
   let (logger_before, logger_after) = Logger::new(None);
