992b275c2580f8ad7efdd6e5d84e281fb13e4075
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2017-07-04 15:08:23 -0500

    Add router.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
 extern crate iron;
-#[macro_use]
-extern crate log;
+#[macro_use] extern crate log;
 extern crate logger;
+extern crate router;
 extern crate simple_logger;
 
 use iron::prelude::Chain;
 use iron::prelude::Iron;
+use iron::prelude::IronResult;
 use iron::prelude::Request;
 use iron::prelude::Response;
 use log::LogLevel;
 use logger::Logger;
+use router::Router;
+
+fn handler(_: &mut Request) -> IronResult<Response> {
+  Ok(Response::with((iron::status::Ok, "Hello world!")))
+}
 
 fn main() {
-    simple_logger::init_with_level(LogLevel::Info).unwrap();
+  simple_logger::init_with_level(LogLevel::Info).unwrap();
 
-    let (logger_before, logger_after) = Logger::new(None);
+  let mut router = Router::new();
 
+  router.get("/", handler, "index");
 
-    let handler = |_: &mut Request| {
-        Ok(Response::with((iron::status::Ok, "Hello world!")))
-    };
+  let (logger_before, logger_after) = Logger::new(None);
 
-    let mut chain = Chain::new(handler);
-    chain.link_before(logger_before);
-    chain.link_after(logger_after);
+  let mut chain = Chain::new(router);
+  chain.link_before(logger_before);
+  chain.link_after(logger_after);
 
-    match Iron::new(chain).http("localhost:3000") {
-        Ok(listening) => info!("{:?}", listening),
-        Err(err) => panic!("{:?}", err),
-    }
+  match Iron::new(chain).http("localhost:3000") {
+    Ok(listening) => info!("{:?}", listening),
+    Err(err) => panic!("{:?}", err),
+  }
 }
