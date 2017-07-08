00cbfb2a1d6b0dd7b14215e8b24e051404ed73e9
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2017-07-08 07:12:12 -0500

    work on command info

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate logger;
 extern crate router;
 extern crate simple_logger;
 
+use iron::Handler;
 use iron::headers::ContentType;
 use iron::modifiers::Header;
 use iron::prelude::Chain;
@@ -15,6 +16,20 @@ use log::LogLevel;
 use logger::Logger;
 use router::Router;
 use std::process::Command;
+
+#[derive(Debug, Clone)]
+struct CommandInfo {
+  http_path: &'static str,
+  command: &'static str,
+  arguments: Vec<&'static str>,
+  description: &'static str
+}
+
+fn get_commands() -> Vec<CommandInfo> {
+  return vec![
+    CommandInfo { http_path: "/ls", command: "ls", arguments: vec!["-l"], description: "ls -l" }
+  ]
+}
 
 fn index_handler(_: &mut Request) -> IronResult<Response> {
   let mut s = String::new();
@@ -28,31 +43,56 @@ fn index_handler(_: &mut Request) -> IronResult<Response> {
   return Ok(Response::with((iron::status::Ok, Header(ContentType::html()), s)));
 }
 
-fn ls_handler(_: &mut Request) -> IronResult<Response> {
-  let command_output =
-    match Command::new("ls").arg("-l").output() {
-      Ok(output) => String::from_utf8_lossy(&output.stdout).into_owned(),
-      Err(err) => format!("command error: {}", err),
-    };
-  let mut s = String::new();
-  s.push_str("<html>");
-  s.push_str("<head><title>ls</title></head>");
-  s.push_str("<body>");
-  s.push_str("<pre>");
-  s.push_str("$ ls -l\n\n");
-  s.push_str(&command_output);
-  s.push_str("</pre>");
-  s.push_str("</body>");
-  s.push_str("</html>");
-  return Ok(Response::with((iron::status::Ok, Header(ContentType::html()), s)));
+struct CommandHandler {
+  command_info: CommandInfo
+}
+
+impl Handler for CommandHandler {
+  fn handle(&self, _: &mut Request) -> IronResult<Response> {
+    let mut command = Command::new(self.command_info.command);
+    let mut args_string = String::new();
+    for arg in self.command_info.arguments.iter() {
+      command.arg(arg);
+      args_string.push_str(arg);
+      args_string.push(' ');
+    }
+    let command_output =
+      match command.output() {
+        Ok(output) => String::from_utf8_lossy(&output.stdout).into_owned(),
+        Err(err) => format!("command error: {}", err),
+      };
+    let mut s = String::new();
+    s.push_str("<html>");
+    s.push_str("<head><title>");
+    s.push_str(&self.command_info.description);
+    s.push_str("</title></head>");
+    s.push_str("<body>");
+    s.push_str("<pre>");
+    s.push_str("$ ");
+    s.push_str(&self.command_info.command);
+    s.push(' ');
+    s.push_str(&args_string);
+    s.push_str("\n\n");
+    s.push_str(&command_output);
+    s.push_str("</pre>");
+    s.push_str("</body>");
+    s.push_str("</html>");
+    return Ok(Response::with((iron::status::Ok, Header(ContentType::html()), s)));
+  }
 }
 
 fn main() {
   simple_logger::init_with_level(LogLevel::Info).unwrap();
 
+  let commands = get_commands();
+
   let mut router = Router::new();
   router.get("/", index_handler, "index");
-  router.get("/ls", ls_handler, "ls");
+
+  for command_info in commands.iter() {
+    let handler = CommandHandler { command_info: command_info.clone() };
+    router.get(command_info.http_path, handler, command_info.description);
+  }
 
   let mut chain = Chain::new(router);
   let (logger_before, logger_after) = Logger::new(None);
